07f6148e1ffb3f8a41553d20505b455b5fb969b3
秋风扫落叶 <songyuequangit@163.com>
2018-05-04 17:25:23 +0800

    服务消费者

---
+++ service-ribbon/src/main/java/org/leazen/Service/HelloService.java
@@ -0,0 +1,20 @@
+package org.leazen.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by songyuequan on 2018/5/4.
+ */
+@Service
+public class HelloService {
+
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+}
---
+++ service-ribbon/src/main/java/org/leazen/controller/HelloController.java
@@ -0,0 +1,22 @@
+package org.leazen.controller;
+
+import org.leazen.Service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by songyuequan on 2018/5/4.
+ */
+@RestController
+public class HelloController {
+
+
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+}
