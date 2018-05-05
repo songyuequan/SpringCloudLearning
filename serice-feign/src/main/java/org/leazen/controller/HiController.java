417ff20f51466c053420a414960a0b0a25b04423
秋风扫落叶 <songyuequangit@163.com>
2018-05-05 15:43:53 +0800

    添加fegin

---
+++ serice-feign/src/main/java/org/leazen/controller/HiController.java
@@ -0,0 +1,23 @@
+package org.leazen.controller;
+
+import org.leazen.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by songyuequan on 2018/5/5.
+ */
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
---
+++ serice-feign/src/main/java/org/leazen/service/SchedualServiceHi.java
@@ -0,0 +1,15 @@
+package org.leazen.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by songyuequan on 2018/5/5.
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
